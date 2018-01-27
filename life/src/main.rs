2603bf3446e8c66b6ef192094536de2470a19296
JanO Mnemonix <jmnemonix@sl9.eu>
2018-01-27 13:48:28 +0100

    load/save files, args, todo ...

--- life/src/main.rs
+++ life/src/main.rs
@@ -1,7 +1,16 @@
-extern crate rand;
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::io::stdin;
+use std::path::Path;
+use std::process;
+use std::thread;
+use std::time;
 
-use std::{thread, time, env};
 use rand::Rng;
+
+extern crate rand;
 
 static MATRIX: &'static [(i8, i8)] =
 &[
@@ -19,29 +28,64 @@ fn init_rd() -> Vec<Vec<bool>> {
 
     for row in 0..height {
         let mut row_vec = Vec::new();
+
         for col in 0..width {
             if rng.gen() {
-                row_vec.push(false)
+                row_vec.push(true);
             } else {
-                row_vec.push(true)
+                row_vec.push(false);
             }
         }
         board.push(row_vec);
     }
     board
 }
+fn load_board_from_file(name: &String) -> Vec<Vec<bool>> {
+    let file = File::open(name).unwrap();
+    let mut board = Vec::new();
+    let mut content = BufReader::new(file);
 
-fn draw(board: &Vec<Vec<bool>>) {
+    for (i,line) in content.lines().enumerate() {
+        let l = line.unwrap();
+        let mut row_vec = Vec::new();
+
+        for c in l.chars() {
+            if c == '#' {
+                row_vec.push(true);
+            } else {
+                row_vec.push(false);
+            }
+        }
+        board.push(row_vec);
+    }
+    board
+}
+fn save_board_to_file(text: &String, name: &String) {
+    let path = Path::new(name);
+
+    let mut file = match File::create(&path) {
+        Err(e) => panic!("No no File!!!"),
+        Ok(file) => file,
+    };
+    match file.write_all(text.as_bytes()) {
+        Err(e) => panic!("No no Write!!!"),
+        Ok(_) => println!("Wrote File!")
+    };
+}
+fn draw(board: &Vec<Vec<bool>>) -> String {
+    let mut result = String::new();
+
     for row in board {
         for col in row {
             if col == &false {
-                print!(" ");
+                result.push(' ');
             } else {
-                print!("#");
+                result.push('#');
             }
         }
-        print!("\n");
+        result.push('\n');
     }
+    result
 }
 fn get_num_neighbours(board: &Vec<Vec<bool>>, cord: (usize,usize)) -> u8 {
     let mut num = 0u8;
@@ -66,7 +110,6 @@ fn get_num_neighbours(board: &Vec<Vec<bool>>, cord: (usize,usize)) -> u8 {
         if x >= board.len() as i8 {
             x = 0 as i8;
         }
-
         if board[x as usize][y as usize] {
             num += 1;
         }
@@ -91,7 +134,6 @@ fn is_cell_alive(board: &Vec<Vec<bool>>, cord: (usize,usize)) -> bool {
 }
 
 fn update(board: Vec<Vec<bool>>) -> Vec<Vec<bool>> {
-
     let mut new_board = Vec::new();
 
     for (r_num, row) in board.iter().enumerate() {
@@ -108,20 +150,113 @@ fn update(board: Vec<Vec<bool>>) -> Vec<Vec<bool>> {
     new_board
 }
 
-fn main() {
-    let mut board = init_rd();
+fn print_help() {
+    println!("Game of Life\n");
+    println!("Implemented in Rust by @svenstaro and @jmnx\n");
+    println!("This is the Help\nCommands:");
+    println!("  --help -h            this help");
+    println!("  --load <filename>    load seed from file and runs it");
+    println!("  --save <filename>    save seed to file and exit");
+    println!("  --random             generates random seed and runs it");
+    println!("  --interactive        startes in interactive mode");
+    println!("  --todo               print a list of TODOs :-O");
+}
 
+fn run_it(init_board: Vec<Vec<bool>>) {
     let mut i = 0;
+    let mut board: Vec<Vec<bool>> = init_board;
 
     loop {
         i += 1;
         println!("{}[2J", 27 as char);
-        draw(&board);
+        print!("{}", draw(&board));
         println!("\n------------------------------------\n Step: {} \n", i);
 
-        let ten_milis = time::Duration::from_millis(100);
-        thread::sleep(ten_milis);
+        let milis = time::Duration::from_millis(100);
+        thread::sleep(milis);
 
         board = update(board);
+    }
+}
+
+fn interactive () {
+    let mut input = String::new();
+    let run = true;
+    while(run) {
+        println!("commands: [load/generate/save]");
+        match stdin().read_line(&mut input) {
+            Err(e) => {
+                println!("Error");
+                exit();
+            }
+            Ok(n) => if input != "load\n" {
+                println!("{} <- not y", input);
+                exit();
+            }
+        }
+    }
+}
+
+fn todos() {
+    println!("TODOs:");
+    println!("* clear warnings");
+    println!("* clean up");
+    println!("* interactive");
+    println!("* object orientation");
+    println!("* by step save");
+    println!("* image generator");
+    println!("* colors");
+    println!("* config. for dead/alive cells");
+    println!("* GoL Webserver :-P");
+    println!("* GoL on the Blockchain :-P");
+}
+
+fn exit () {
+    process::exit(0x0f00);
+}
+
+fn main() {
+    let mut board: Vec<Vec<bool>>;
+    let args: Vec<String> = env::args().collect();
+    let command: String;
+    let file_name: String;
+    let extra_arg: String;
+
+    if args.len() >= 2 {
+
+        command = args[1].to_string();
+        if args.len() >= 3 {
+            file_name = args[2].to_string();
+            extra_arg = args[3].to_string();
+
+            println!("Args: {} {} {} \n", command, file_name, extra_arg);
+
+            if command == "--load" {
+                board = load_board_from_file( &file_name );
+                run_it(board);
+            } else if command == "--save" {
+                board = init_rd();
+                save_board_to_file( &draw(&board), &file_name );
+                exit();
+            }
+        } else {
+            println!("Args: {} \n", command);
+        }
+
+        if command == "--help" || command == "-h" {
+            print_help();
+            exit();
+        } else if command == "--random" {
+            board = init_rd();
+            run_it(board);
+        } else if command == "--interactive" {
+            println!("TODO");
+        } else if command == "--todos" {
+            todos();
+            exit();
+        }
+
+        if args.len() >= 3 {
+        }
     }
 }
